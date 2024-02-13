8d74fbfe1847f80f5dadf072381d065a09b470b5
jimyungkoh <kojimyung1206@naver.com>
2024-02-13 14:15:50 +0900

    feat: header 내 brands 페이지 링크 연결

--- src/app/_common/Header/Header.tsx
+++ src/app/_common/Header/Header.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 export default function Header() {
-
   return (
     <header className="bg-white sticky top-0 h-16 border-b flex items-center px-5 lg:px-8 z-10 shrink-0">
       <Link href="/" className="font-bold text-2xl">
@@ -9,7 +8,7 @@ export default function Header() {
       </Link>
       <nav className="ml-20">
         <ul>
-          <li>BRANDS</li>
+          <li><Link href="/brands">BRANDS</Link></li>
         </ul>
       </nav>
       <div className="ml-auto flex items-center gap-x-4">
