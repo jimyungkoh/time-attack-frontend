1f7026367d507ed4d25e41dcd17e7bd6e87ed9c9
jimyungkoh <kojimyung1206@naver.com>
2024-02-13 13:26:06 +0900

    feat: moneyFormatter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import DefaultLayout from "@/app/_common/DefaultLayout";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Providers from "./_providers";
+import Providers from "./_providers/Providers";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
---
+++ src/types/brand.type.ts
@@ -0,0 +1,5 @@
+export type Brand = {
+  id: number;
+  nameKr: string | undefined;
+  nameEn: string | undefined;
+};
